beb3dc135a04868c9ac5a5fa331157808757e381
Peter Adelodun <adelodunpeter69@gmail.com>
2025-11-19 14:22:58 +0100

    Disable sandbox to enable preload script for system metrics

--- src/db-toolkit-ui/electron/main.js
+++ src/db-toolkit-ui/electron/main.js
@@ -95,7 +95,8 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      sandbox: false
     },
   });
   
