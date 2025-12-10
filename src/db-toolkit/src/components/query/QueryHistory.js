3d070be1c4538d85d19d45179f98ad13cf35720a
Adelodun Peter <adelodunpeter69@gmail.com>
2025-12-10 13:31:17 +0100

    Recreate QueryHistory to fix parsing error

--- src/db-toolkit/src/components/query/QueryHistory.js
+++ src/db-toolkit/src/components/query/QueryHistory.js
@@ -73,7 +73,7 @@ export function QueryHistory({ connectionId, onSelectQuery }) {
                 </button>
               )}
             </div>
-          ))
+          ))}
         </div>
       )}
     </div>
