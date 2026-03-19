97e5f7fa679a461131df02ee018630565e8cb321
Adelodun Peter <adelodunpeter69@gmail.com>
2026-03-19 17:11:38 +0100

    fix: add missing Database icon import in QueryEditorSelectPage

--- src/db-toolkit/src/pages/QueryEditorSelectPage.js
+++ src/db-toolkit/src/pages/QueryEditorSelectPage.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Database } from 'lucide-react';
 import { connectionsAPI } from '../services/api';
 import { useToast } from '../contexts/ToastContext';
 import { LoadingState } from '../components/common/LoadingState';
